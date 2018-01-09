87f49ab754a14d1c90d7dfdb879480a35e082c78
lx3238443k <lx3238443k@126.com>
2018-01-09 18:34:49 +0800

    todo-end

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 $(document).ready(function(){
  
 
-  var that_active = $('#side-list');
+  let that_active = $('#side-list');
+
+  function randTitle(num){
+    
+    let a='未命名标题';
+    return a+num;
+
+  }
+
+  let num =0;
 
   $('#side-list').on('click', 'li', function(ev) {
     // this 指向委托的对象 li
     that_active.toggleClass('active');
     if(this.id==='creat-list'){
-      $(this).before('<li class="todo-list active"></li>');
+      num++;
+      let title = randTitle(num);
+      $(this).before(`<li class="todo-list active">
+                        <div class="list-logo">
+                            <i class="icon-list"></i>
+                        </div>
+                        <div class="list-title">${title}</div>
+                        <div class="list-num"></div>
+                      </li>`);
       that_active = $(this).prev();
     }
     else{
