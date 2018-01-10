c6ef76dec677c0f4ae322a775dfb227dd6354779
lx3238443k <lx3238443k@126.com>
2018-01-10 17:43:44 +0800

    重新整合

---
+++ js/main.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+ 
+  $(document).bind("contextmenu", function() { return false; });
+  let that_active = $('#side-list');
+
+  function randTitle(num){
+    
+    let a='未命名标题';
+    return a+num;
+
+  }
+
+  let num =0;
+  // 左侧栏事件监听
+  $('#side-list').on('mouseup', 'li', function(ev) {
+    // this 指向委托的对象 li
+
+    if(ev.which===1||ev.button==0){
+          that_active.toggleClass('active');
+          if(this.id==='creat-list'){
+            num++;
+            let title = randTitle(num);
+            $(this).before(`<li class="todo-list active" data-listnum="${num}">
+                              <div class="list-logo">
+                                  <i class="icon-list"></i>
+                              </div>
+                              <div class="list-title">${title}</div>
+                              <div class="list-num"></div>
+                            </li>`);
+            that_active = $(this).prev();
+            $(this).prev().css("opacity","0");
+            setTimeout(()=>{
+              $(this).prev().css("opacity","1");
+            },100)
+          }
+          else{
+            $(this).toggleClass('active');
+            that_active = $(this);
+          }
+
+    }
+
+    if(ev.which===3||ev.button==2){
+
+      $('#dialog').css({
+        "display":"block",
+        "top":ev.clientY,
+        "left":ev.clientX
+      });
+
+      $('#dialog .dialog-inner').append(` <li>
+          <i class="icon-pause"></i>
+                <span class="dialog-text">复制</span>
+            </li>
+            <li>
+                <i class="icon-copy"></i>
+                <span class="dialog-text">粘贴</span>
+            </li>`);
+
+      $('#dialog .dialog-warrin').append(`<li id="del-btn" data-dellist="${ev.currentTarget.dataset.listnum
+      }">
+      <label for="del-btn">
+      <i class="icon-del"></i>
+      <span class="dialog-text">删除</span></label>
+        </li>`);
+
+
+      
+
+    }
+   
+    });
+  
+
+    $('.left').on('mousedown', 'li', (e)=>{
+       clearDialog();
+      });
+   
+
+      $('#dialog').on('click','li',(e)=>{
+
+        console.log(e.target.dataset.dellist);
+
+        $(`#side-list li[data-listnum="${e.target.dataset.dellist}"]`).remove();  
+        clearDialog();
+      });
+
+    function clearDialog(){
+      $('#dialog').css({
+        "display":"none"
+      });
+
+
+      $('#dialog .dialog-inner li').remove();
+      $('#dialog .dialog-warrin li').remove();
+    }
+
+
+});
